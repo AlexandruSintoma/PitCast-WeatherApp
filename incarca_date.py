f6e035c0f54e17bc6b0031bdf3cb5e2f8553d39c
Alexandru Sintoma <alexandru.sintoma@gmail.com>
2026-07-04 10:40:22 +0300

    Adaugat toate cele 24 de circuite din calendarul F1 2026

--- incarca_date.py
+++ incarca_date.py
@@ -1,5 +1,6 @@
 # scriptul asta il rulez o singura data ca sa pun circuitele in baza de date
 # ca sa nu le adaug pe toate de mana din admin
+# sunt toate cele 24 de curse din calendarul de formula 1 din 2026, in ordine
 # il pornesc cu: python incarca_date.py
 
 import os
@@ -16,48 +17,8 @@ from circuite import models
 models.Circuit.objects.all().delete()
 
 
-# Bahrain
+# Runda 1 - Australia
 circuit1 = models.Circuit.objects.create(
-    nume='Bahrain International Circuit',
-    tara='Bahrain',
-    oras='Sakhir',
-    latitudine=26.0325,
-    longitudine=50.5106,
-    lungime_km=5.412,
-    numar_viraje=15,
-    an_prima_cursa=2004,
-    istorie='Construit in mijlocul desertului, este locul unde de multe ori incepe sezonul. Se alearga si seara, sub lumini, iar nisipul de langa pista face totul mai spectaculos.',
-    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Bahrain',
-)
-models.MarePremiu.objects.create(
-    circuit=circuit1,
-    nume='Marele Premiu al Bahrainului',
-    runda=1,
-    data_cursei=date(2026, 3, 8),
-)
-
-# Arabia Saudita
-circuit2 = models.Circuit.objects.create(
-    nume='Jeddah Corniche Circuit',
-    tara='Arabia Saudita',
-    oras='Jeddah',
-    latitudine=21.6319,
-    longitudine=39.1044,
-    lungime_km=6.174,
-    numar_viraje=27,
-    an_prima_cursa=2021,
-    istorie='Este cel mai rapid circuit de strada din calendar. Trece pe langa malul marii si are foarte multe viraje rapide, unde nu prea ai loc de greseli.',
-    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Jeddah',
-)
-models.MarePremiu.objects.create(
-    circuit=circuit2,
-    nume='Marele Premiu al Arabiei Saudite',
-    runda=2,
-    data_cursei=date(2026, 3, 15),
-)
-
-# Australia
-circuit3 = models.Circuit.objects.create(
     nume='Albert Park Circuit',
     tara='Australia',
     oras='Melbourne',
@@ -70,14 +31,34 @@ circuit3 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Melbourne',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit3,
+    circuit=circuit1,
     nume='Marele Premiu al Australiei',
-    runda=3,
-    data_cursei=date(2026, 3, 29),
+    runda=1,
+    data_cursei=date(2026, 3, 8),
 )
 
-# Japonia
-circuit4 = models.Circuit.objects.create(
+# Runda 2 - China
+circuit2 = models.Circuit.objects.create(
+    nume='Shanghai International Circuit',
+    tara='China',
+    oras='Shanghai',
+    latitudine=31.3389,
+    longitudine=121.22,
+    lungime_km=5.451,
+    numar_viraje=16,
+    an_prima_cursa=2004,
+    istorie='Are un viraj lung in forma de melc chiar la inceput, care se strange tot mai mult. Prima cursa aici a fost in 2004 si are o linie dreapta foarte lunga.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Shanghai',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit2,
+    nume='Marele Premiu al Chinei',
+    runda=2,
+    data_cursei=date(2026, 3, 15),
+)
+
+# Runda 3 - Japonia
+circuit3 = models.Circuit.objects.create(
     nume='Suzuka International Racing Course',
     tara='Japonia',
     oras='Suzuka',
@@ -90,14 +71,54 @@ circuit4 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Suzuka',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit4,
+    circuit=circuit3,
     nume='Marele Premiu al Japoniei',
+    runda=3,
+    data_cursei=date(2026, 3, 29),
+)
+
+# Runda 4 - Bahrain
+circuit4 = models.Circuit.objects.create(
+    nume='Bahrain International Circuit',
+    tara='Bahrain',
+    oras='Sakhir',
+    latitudine=26.0325,
+    longitudine=50.5106,
+    lungime_km=5.412,
+    numar_viraje=15,
+    an_prima_cursa=2004,
+    istorie='Construit in mijlocul desertului, este un circuit iubit de echipe pentru ca aici se fac multe teste. Se alearga seara, sub lumini, iar nisipul de langa pista face totul mai spectaculos.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Bahrain',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit4,
+    nume='Marele Premiu al Bahrainului',
     runda=4,
     data_cursei=date(2026, 4, 12),
 )
 
-# Statele Unite - Miami
+# Runda 5 - Arabia Saudita
 circuit5 = models.Circuit.objects.create(
+    nume='Jeddah Corniche Circuit',
+    tara='Arabia Saudita',
+    oras='Jeddah',
+    latitudine=21.6319,
+    longitudine=39.1044,
+    lungime_km=6.174,
+    numar_viraje=27,
+    an_prima_cursa=2021,
+    istorie='Este cel mai rapid circuit de strada din calendar. Trece pe langa malul marii si are foarte multe viraje rapide, unde nu prea ai loc de greseli.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Jeddah',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit5,
+    nume='Marele Premiu al Arabiei Saudite',
+    runda=5,
+    data_cursei=date(2026, 4, 19),
+)
+
+# Runda 6 - Statele Unite (Miami)
+circuit6 = models.Circuit.objects.create(
     nume='Miami International Autodrome',
     tara='Statele Unite',
     oras='Miami',
@@ -110,54 +131,14 @@ circuit5 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Miami',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit5,
+    circuit=circuit6,
     nume='Marele Premiu al orasului Miami',
-    runda=5,
+    runda=6,
     data_cursei=date(2026, 5, 3),
 )
 
-# Monaco
-circuit6 = models.Circuit.objects.create(
-    nume='Circuit de Monaco',
-    tara='Monaco',
-    oras='Monte Carlo',
-    latitudine=43.7347,
-    longitudine=7.4206,
-    lungime_km=3.337,
-    numar_viraje=19,
-    an_prima_cursa=1950,
-    istorie='Este cea mai celebra cursa din tot sezonul. Se merge pe strazile inguste din Monte Carlo, printre cladiri si langa port. Depasirile sunt aproape imposibile aici.',
-    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Monaco',
-)
-models.MarePremiu.objects.create(
-    circuit=circuit6,
-    nume='Marele Premiu de la Monaco',
-    runda=6,
-    data_cursei=date(2026, 5, 24),
-)
-
-# Spania
+# Runda 7 - Canada
 circuit7 = models.Circuit.objects.create(
-    nume='Circuit de Barcelona-Catalunya',
-    tara='Spania',
-    oras='Barcelona',
-    latitudine=41.57,
-    longitudine=2.2611,
-    lungime_km=4.657,
-    numar_viraje=16,
-    an_prima_cursa=1991,
-    istorie='Este circuitul pe care echipele il cunosc cel mai bine, pentru ca aici se fac multe teste inainte de sezon. Are de toate: viraje rapide, viraje lente si o linie dreapta lunga.',
-    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Barcelona',
-)
-models.MarePremiu.objects.create(
-    circuit=circuit7,
-    nume='Marele Premiu al Spaniei',
-    runda=7,
-    data_cursei=date(2026, 6, 7),
-)
-
-# Canada
-circuit8 = models.Circuit.objects.create(
     nume='Circuit Gilles Villeneuve',
     tara='Canada',
     oras='Montreal',
@@ -170,14 +151,74 @@ circuit8 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Montreal',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit8,
+    circuit=circuit7,
     nume='Marele Premiu al Canadei',
+    runda=7,
+    data_cursei=date(2026, 5, 24),
+)
+
+# Runda 8 - Monaco
+circuit8 = models.Circuit.objects.create(
+    nume='Circuit de Monaco',
+    tara='Monaco',
+    oras='Monte Carlo',
+    latitudine=43.7347,
+    longitudine=7.4206,
+    lungime_km=3.337,
+    numar_viraje=19,
+    an_prima_cursa=1950,
+    istorie='Este cea mai celebra cursa din tot sezonul. Se merge pe strazile inguste din Monte Carlo, printre cladiri si langa port. Depasirile sunt aproape imposibile aici.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Monaco',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit8,
+    nume='Marele Premiu de la Monaco',
     runda=8,
+    data_cursei=date(2026, 6, 7),
+)
+
+# Runda 9 - Spania (Barcelona)
+circuit9 = models.Circuit.objects.create(
+    nume='Circuit de Barcelona-Catalunya',
+    tara='Spania',
+    oras='Barcelona',
+    latitudine=41.57,
+    longitudine=2.2611,
+    lungime_km=4.657,
+    numar_viraje=16,
+    an_prima_cursa=1991,
+    istorie='Este circuitul pe care echipele il cunosc cel mai bine, pentru ca aici se fac multe teste inainte de sezon. Are de toate: viraje rapide, viraje lente si o linie dreapta lunga.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Barcelona',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit9,
+    nume='Marele Premiu al Spaniei',
+    runda=9,
     data_cursei=date(2026, 6, 14),
 )
 
-# Marea Britanie
-circuit9 = models.Circuit.objects.create(
+# Runda 10 - Austria
+circuit10 = models.Circuit.objects.create(
+    nume='Red Bull Ring',
+    tara='Austria',
+    oras='Spielberg',
+    latitudine=47.2197,
+    longitudine=14.7647,
+    lungime_km=4.318,
+    numar_viraje=10,
+    an_prima_cursa=1970,
+    istorie='Este un circuit scurt, asezat printre dealurile din Austria. Se termina repede un tur, asa ca masinile trec des unele pe langa altele.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Spielberg',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit10,
+    nume='Marele Premiu al Austriei',
+    runda=10,
+    data_cursei=date(2026, 6, 28),
+)
+
+# Runda 11 - Marea Britanie
+circuit11 = models.Circuit.objects.create(
     nume='Silverstone Circuit',
     tara='Marea Britanie',
     oras='Silverstone',
@@ -190,14 +231,14 @@ circuit9 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Silverstone',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit9,
+    circuit=circuit11,
     nume='Marele Premiu al Marii Britanii',
-    runda=9,
+    runda=11,
     data_cursei=date(2026, 7, 5),
 )
 
-# Belgia
-circuit10 = models.Circuit.objects.create(
+# Runda 12 - Belgia
+circuit12 = models.Circuit.objects.create(
     nume='Circuit de Spa-Francorchamps',
     tara='Belgia',
     oras='Spa',
@@ -210,14 +251,54 @@ circuit10 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Spa',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit10,
+    circuit=circuit12,
     nume='Marele Premiu al Belgiei',
-    runda=10,
+    runda=12,
+    data_cursei=date(2026, 7, 19),
+)
+
+# Runda 13 - Ungaria
+circuit13 = models.Circuit.objects.create(
+    nume='Hungaroring',
+    tara='Ungaria',
+    oras='Budapesta',
+    latitudine=47.5789,
+    longitudine=19.2486,
+    lungime_km=4.381,
+    numar_viraje=14,
+    an_prima_cursa=1986,
+    istorie='Este un circuit stramt si plin de viraje, unde e greu sa depasesti. De multe ori e foarte cald aici in mijlocul verii.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Budapesta',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit13,
+    nume='Marele Premiu al Ungariei',
+    runda=13,
     data_cursei=date(2026, 7, 26),
 )
 
-# Italia
-circuit11 = models.Circuit.objects.create(
+# Runda 14 - Olanda
+circuit14 = models.Circuit.objects.create(
+    nume='Circuit Zandvoort',
+    tara='Olanda',
+    oras='Zandvoort',
+    latitudine=52.3888,
+    longitudine=4.5409,
+    lungime_km=4.259,
+    numar_viraje=14,
+    an_prima_cursa=1952,
+    istorie='Este langa mare, printre dune de nisip din Olanda. Are viraje inclinate, ca la ovalele americane, care ajuta masinile sa treaca mai repede prin ele.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Zandvoort',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit14,
+    nume='Marele Premiu al Olandei',
+    runda=14,
+    data_cursei=date(2026, 8, 23),
+)
+
+# Runda 15 - Italia
+circuit15 = models.Circuit.objects.create(
     nume='Autodromo Nazionale Monza',
     tara='Italia',
     oras='Monza',
@@ -230,14 +311,54 @@ circuit11 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Monza',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit11,
+    circuit=circuit15,
     nume='Marele Premiu al Italiei',
-    runda=11,
+    runda=15,
     data_cursei=date(2026, 9, 6),
 )
 
-# Singapore
-circuit12 = models.Circuit.objects.create(
+# Runda 16 - Spania (Madrid), circuit nou in 2026
+circuit16 = models.Circuit.objects.create(
+    nume='Madring',
+    tara='Spania',
+    oras='Madrid',
+    latitudine=40.4637,
+    longitudine=-3.6167,
+    lungime_km=5.474,
+    numar_viraje=22,
+    an_prima_cursa=2026,
+    istorie='Este cel mai nou circuit din calendar si debuteaza chiar in 2026. Este un amestec de strazi si portiuni de circuit, langa targul IFEMA din Madrid.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Madrid',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit16,
+    nume='Marele Premiu al Madridului',
+    runda=16,
+    data_cursei=date(2026, 9, 13),
+)
+
+# Runda 17 - Azerbaidjan
+circuit17 = models.Circuit.objects.create(
+    nume='Baku City Circuit',
+    tara='Azerbaidjan',
+    oras='Baku',
+    latitudine=40.3725,
+    longitudine=49.8533,
+    lungime_km=6.003,
+    numar_viraje=20,
+    an_prima_cursa=2016,
+    istorie='Este un circuit de strada prin orasul vechi. Are o linie dreapta foarte lunga langa mare si o portiune ingusta pe langa un castel vechi.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Baku',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit17,
+    nume='Marele Premiu al Azerbaidjanului',
+    runda=17,
+    data_cursei=date(2026, 9, 27),
+)
+
+# Runda 18 - Singapore
+circuit18 = models.Circuit.objects.create(
     nume='Marina Bay Street Circuit',
     tara='Singapore',
     oras='Singapore',
@@ -250,11 +371,131 @@ circuit12 = models.Circuit.objects.create(
     imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Singapore',
 )
 models.MarePremiu.objects.create(
-    circuit=circuit12,
+    circuit=circuit18,
     nume='Marele Premiu de la Singapore',
-    runda=12,
-    data_cursei=date(2026, 9, 20),
+    runda=18,
+    data_cursei=date(2026, 10, 11),
+)
+
+# Runda 19 - Statele Unite (Austin)
+circuit19 = models.Circuit.objects.create(
+    nume='Circuit of the Americas',
+    tara='Statele Unite',
+    oras='Austin',
+    latitudine=30.1328,
+    longitudine=-97.6411,
+    lungime_km=5.513,
+    numar_viraje=20,
+    an_prima_cursa=2012,
+    istorie='Are un deal abrupt chiar la primul viraj, de unde ai o priveliste peste tot circuitul. Cateva viraje sunt copiate dupa cele mai tari viraje din alte circuite din lume.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Austin',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit19,
+    nume='Marele Premiu al Statelor Unite',
+    runda=19,
+    data_cursei=date(2026, 10, 25),
+)
+
+# Runda 20 - Mexic
+circuit20 = models.Circuit.objects.create(
+    nume='Autodromo Hermanos Rodriguez',
+    tara='Mexic',
+    oras='Ciudad de Mexico',
+    latitudine=19.4042,
+    longitudine=-99.0907,
+    lungime_km=4.304,
+    numar_viraje=17,
+    an_prima_cursa=1963,
+    istorie='Este asezat foarte sus fata de nivelul marii, iar aerul rarefiat face masinile sa se comporte ciudat. Are o portiune spectaculoasa care trece printr-un stadion plin de fani.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Mexico+City',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit20,
+    nume='Marele Premiu al orasului Mexico',
+    runda=20,
+    data_cursei=date(2026, 11, 1),
+)
+
+# Runda 21 - Brazilia
+circuit21 = models.Circuit.objects.create(
+    nume='Autodromo Jose Carlos Pace (Interlagos)',
+    tara='Brazilia',
+    oras='Sao Paulo',
+    latitudine=-23.7036,
+    longitudine=-46.6997,
+    lungime_km=4.309,
+    numar_viraje=15,
+    an_prima_cursa=1973,
+    istorie='Se merge invers fata de acele ceasului si pista urca si coboara tot timpul. Aici s-au vazut unele dintre cele mai nebune curse din istorie, mai ales cand ploua.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Sao+Paulo',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit21,
+    nume='Marele Premiu al orasului Sao Paulo',
+    runda=21,
+    data_cursei=date(2026, 11, 8),
+)
+
+# Runda 22 - Statele Unite (Las Vegas)
+circuit22 = models.Circuit.objects.create(
+    nume='Las Vegas Strip Circuit',
+    tara='Statele Unite',
+    oras='Las Vegas',
+    latitudine=36.1147,
+    longitudine=-115.1728,
+    lungime_km=6.201,
+    numar_viraje=17,
+    an_prima_cursa=2023,
+    istorie='Se alearga noaptea chiar pe faimosul bulevard Strip, printre cazinouri si lumini. Masinile ating viteze foarte mari pe liniile drepte lungi.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Las+Vegas',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit22,
+    nume='Marele Premiu de la Las Vegas',
+    runda=22,
+    data_cursei=date(2026, 11, 21),
+)
+
+# Runda 23 - Qatar
+circuit23 = models.Circuit.objects.create(
+    nume='Lusail International Circuit',
+    tara='Qatar',
+    oras='Lusail',
+    latitudine=25.49,
+    longitudine=51.4542,
+    lungime_km=5.419,
+    numar_viraje=16,
+    an_prima_cursa=2021,
+    istorie='Se alearga noaptea sub lumini, in desert. Este un circuit rapid, cu multe viraje lungi care ii obosesc mult pe piloti.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Lusail',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit23,
+    nume='Marele Premiu al Qatarului',
+    runda=23,
+    data_cursei=date(2026, 11, 29),
+)
+
+# Runda 24 - Emiratele Arabe Unite (Abu Dhabi)
+circuit24 = models.Circuit.objects.create(
+    nume='Yas Marina Circuit',
+    tara='Emiratele Arabe Unite',
+    oras='Abu Dhabi',
+    latitudine=24.4672,
+    longitudine=54.6031,
+    lungime_km=5.281,
+    numar_viraje=16,
+    an_prima_cursa=2009,
+    istorie='De obicei aici se incheie sezonul. Cursa incepe la apus si se termina noaptea, iar circuitul trece pe langa un hotel spectaculos si un port cu iahturi.',
+    imagine_url='https://placehold.co/600x340/15151e/e10600/png?text=Abu+Dhabi',
+)
+models.MarePremiu.objects.create(
+    circuit=circuit24,
+    nume='Marele Premiu de la Abu Dhabi',
+    runda=24,
+    data_cursei=date(2026, 12, 6),
 )
 
 
-print('Gata! Am adaugat toate circuitele in baza de date.')
+print('Gata! Am adaugat toate cele 24 de circuite in baza de date.')
